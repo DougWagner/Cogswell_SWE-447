555f7ecf04f30be63c22cb9b3361dbd547a01f99
Doug Wagner <digwagner@gmail.com>
2016-02-09 20:02:01 -0800

    moved initShaders.js and webgl-utils.js to common directory - added cube.js

---
+++ hw2/cube.js
@@ -0,0 +1,22 @@
+var cube = {
+    count : 8,
+    positions : {
+        values : new Float32Array([
+            0.0, 0.0, 0.0, // Vertex 0
+            1.0, 0.0, 0.0, // Vertex 1
+            0.0, 1.0, 0.0, // Vertex 2
+            1.0, 1.0, 0.0, // Vertex 3
+            0.0, 0.0, 1.0, // Vertex 4
+            1.0, 0.0, 1.0, // Vertex 5
+            0.0, 1.0, 1.0, // Vertex 6
+            1.0, 1.0, 1.0  // Vertex 7
+        ]),
+        numComponents : 3
+    },
+    colors : {
+        values : new Float32Array([
+            /* values to come */
+        ]),
+        numComponents : 3
+    }
+};
